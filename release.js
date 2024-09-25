671167162798bbd12ffba65b14f059e6866b44e9
Emam546 <emam54637@gmail.com>
2024-09-25 05:47:58 +0300

    debug release production

--- release.js
+++ release.js
@@ -3,6 +3,7 @@ const path = require("path");
 const packageJson = require("./package.json");
 const files = ["gym-membership-setup-" + packageJson.version + "-win.exe"];
 const fs = require("fs");
+const distFolder = "./dist";
 const assets = files.map((file) => path.join("./dist", file));
 const options = {
   token: process.env.GH_TOKEN,
@@ -23,8 +24,18 @@ const options = {
   assets: assets,
 };
 const missedFile = assets.find((file) => !fs.existsSync(file));
-if (missedFile) throw new Error("missed File");
 console.log(assets);
+if (missedFile)
+  throw new Error(
+    `This file does not exist in the path ${path.join(__dirname, missedFile)} 
+    the current files  are ${JSON.stringify(
+      fs.readdirSync(distFolder).map((file) => path.join(distFolder, file)),
+      undefined,
+      2
+    )}
+    `
+  );
+
 publishRelease(options, function (err, release) {
   if (err) return console.error(err);
   console.log("finish Release");
