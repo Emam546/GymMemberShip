b991f6234caff29d2f381ddaf4b7a0b1944679b6
Emam546 <emam54637@gmail.com>
2024-09-27 01:19:00 +0300

    change autoIndex to true

--- server/src/db/connect.ts
+++ server/src/db/connect.ts
@@ -3,11 +3,11 @@ import EnvVars from "@serv/declarations/major/EnvVars";
 import mongoose from "mongoose";
 export const dbName = "GymMemberShip";
 let cached: mongoose.Mongoose;
-export default async function connect(url: string, autoIndex = false) {
+export default async function connect(url: string, autoIndex = true) {
   if (cached) return cached;
   cached = await mongoose.connect(url, {
     minPoolSize: 10, // Can now run 10 operations at a time
-    autoIndex: true,
+    autoIndex: EnvVars.nodeEnv !== NodeEnvs.Production || autoIndex,
     dbName,
   });
   return cached;
