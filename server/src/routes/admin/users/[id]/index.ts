cef2148d9a3b952d1066697d5582e6b1fe213bd8
Emam546 <emam54637@gmail.com>
2024-09-28 06:39:56 +0300

    remove unused routes

--- server/src/routes/admin/users/[id]/index.ts
+++ server/src/routes/admin/users/[id]/index.ts
@@ -60,24 +60,6 @@ router.delete("/:id", async (req, res) => {
   res.status(200).sendSuccess(newUser);
 });
 
-router.get("/:id/logs", async (req, res) => {
-  const user = res.locals.user as Document<DataBase.Models.User>;
-  const logs = await getLogs(
-    req.query,
-    { userId: user._id },
-    { userId: 1, createdAt: -1 },
-    ["planId", "paymentId"]
-  );
-  res.status(200).sendSuccess(logs);
-});
-router.get("/:id/logs/count", async (req, res) => {
-  const user = res.locals.user as Document<DataBase.Models.User>;
-  const logs = await Logs.countDocuments({ userId: user._id }).hint({
-    userId: 1,
-    createdAt: -1,
-  });
-  res.status(200).sendSuccess(logs);
-});
 router.use("/:id", paymentsRouter);
 router.use("/:id", logsRouter);
 export default router;
