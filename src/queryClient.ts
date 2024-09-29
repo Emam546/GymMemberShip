5330f6e711747724ccb98ba9a077d32d7d84f225
Emam546 <emam54637@gmail.com>
2024-09-29 07:08:53 +0300

    fix the problem if there is no connection problem

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -2,11 +2,15 @@ import { QueryClient } from "@tanstack/react-query";
 
 const cacheTime = 30 * (60 * 1000);
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 10 * (60 * 1000), // 10 mins
-            cacheTime: cacheTime + 5 * (60 * 1000),
-        },
+  defaultOptions: {
+    queries: {
+      staleTime: 10 * (60 * 1000), // 10 mins
+      cacheTime: cacheTime + 5 * (60 * 1000),
+      networkMode: "always",
     },
+    mutations: {
+      networkMode: "always",
+    },
+  },
 });
 export default queryClient;
