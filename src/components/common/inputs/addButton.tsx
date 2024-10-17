53ae2e17cb9c087187c3dca0332e91ea8f462487
Emam546 <emam54637@gmail.com>
2024-10-17 17:41:31 +0300

    add addButton

--- src/components/common/inputs/addButton.tsx
+++ src/components/common/inputs/addButton.tsx
@@ -2,9 +2,10 @@ import { faAdd, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link, { LinkProps } from "next/link";
 import React, { ComponentProps } from "react";
+import classNames from "classnames";
 export type Props = {
   label: string;
-} & LinkProps;
+};
 function Button({ ...props }: LinkProps) {
   return (
     <Link
@@ -13,7 +14,7 @@ function Button({ ...props }: LinkProps) {
     />
   );
 }
-export function GoToButton({ label, ...props }: Props) {
+export function GoToButton({ label, ...props }: Props & LinkProps) {
   return (
     <Button {...props}>
       <FontAwesomeIcon className="tw-rotate-180" icon={faArrowRight} />
@@ -21,11 +22,31 @@ export function GoToButton({ label, ...props }: Props) {
     </Button>
   );
 }
-export default function AddButton({ label, ...props }: Props) {
+export default function GoToAddButton({ label, ...props }: Props & LinkProps) {
   return (
-    <Button {...props}>
+    <Link
+      {...props}
+      className="tw-text-blue-600 tw-font-bold hover:tw-bg-blue-600/10 tw-transition-all tw-w-full tw-py-3 tw-text-start tw-px-4 tw-flex tw-gap-2 tw-items-center"
+    >
       <FontAwesomeIcon icon={faAdd} />
       <span>{label}</span>
-    </Button>
+    </Link>
+  );
+}
+export function AddButton({
+  label,
+  ...props
+}: Props & ComponentProps<"button">) {
+  return (
+    <button
+      {...props}
+      className={classNames(
+        "tw-border-none tw-bg-inherit",
+        "tw-text-blue-600 tw-font-bold hover:tw-bg-blue-600/10 tw-transition-all tw-w-full tw-py-3 tw-text-start tw-px-4 tw-flex tw-gap-2 tw-items-center"
+      )}
+    >
+      <FontAwesomeIcon icon={faAdd} />
+      <span>{label}</span>
+    </button>
   );
 }
