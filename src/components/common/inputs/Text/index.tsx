f15a8d7ee3111141dfbd77caba2e0d1e52835657
Emam546 <emam54637@gmail.com>
2024-10-17 09:55:42 +0300

    fix the problem of text input

--- src/components/common/inputs/Text/index.tsx
+++ src/components/common/inputs/Text/index.tsx
@@ -1,19 +1,29 @@
 import classNames from "classnames";
-import React from "react";
+import React, { useState } from "react";
 import { ComponentProps, ComponentRef } from "react";
 
 export const TextInput = React.forwardRef<
   ComponentRef<"input">,
   ComponentProps<"input">
->(({ className, ...props }, ref) => {
+>(({ className, onChange, ...props }, ref) => {
+  const [textValue, setState] = useState(props.value);
   return (
-    <input
-      className={classNames(
-        "tw-p-0 tw-m-0 tw-border-none ltr:tw-pr-3 rtl:tw-pl-3 tw-w-auto focus:tw-outline focus:tw-outline-2 focus:tw-outline-gray-600 tw-rounded",
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
+    <div
+      className="tw-w-fit tw-relative tw-min-w-[3rem] tw-bg-red-700 tw-p-0 after:tw-content-[attr(data-value)] after:tw-invisible"
+      data-value={textValue}
+    >
+      <input
+        className={classNames(
+          "tw-p-0 tw-m-0 tw-border-none ltr:tw-pr-3 rtl:tw-pl-3 focus:tw-outline focus:tw-outline-2 focus:tw-outline-gray-600 tw-rounded tw-w-full",
+          className
+        )}
+        onChange={(e) => {
+          onChange?.(e);
+          setState(e.currentTarget.value);
+        }}
+        ref={ref}
+        {...props}
+      />
+    </div>
   );
 });
