793bc00f3a083481130329379cd3d22327bb5083
Emam546 <emam54637@gmail.com>
2024-10-18 13:47:53 +0300

    finish adding users to route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import cors from "cors";
 import adminRoute from "./admin";
+import usersRoute from "./users";
 const router = Router();
 
 router.use(cors({ origin: "*" }));
 router.use("/admin", adminRoute);
+router.use("/users", usersRoute);
 export default router;
--- server/src/routes/users/exercises/workout/index.ts
+++ server/src/routes/users/exercises/workout/index.ts
@@ -1,13 +1,8 @@
 import { Router } from "express";
 import IdRouter from "./[id]";
-import Workouts from "@serv/models/workout";
-import { Document, RootFilterQuery } from "mongoose";
+import { Document } from "mongoose";
 const router = Router();
-export async function getAllWorkouts(
-  match: RootFilterQuery<DataBase.Models.Workouts>
-) {
-  return await Workouts.find(match);
-}
+
 router.get("/", (req, res) => {
   const exercise = res.locals.exercise as Document<
     DataBase.Models.Exercises,
